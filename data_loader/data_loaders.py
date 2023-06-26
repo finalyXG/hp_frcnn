865080d958dde4317d6e3c8b4ee47c39a4409649
laurence <laurence0636@outlook.com>
2023-06-26 16:38:02 +0800

    feat: dev code for distributed training

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,6 +1,58 @@
+from pathlib import Path
+from random import Random
+
+import cv2
+import numpy as np
+import torch
+from matplotlib import pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+from matplotlib.patches import Rectangle
+from PIL import Image
+from torch.utils.data import Dataset
 from torchvision import datasets, transforms
+
+import utils.pytorch_vision.transforms as T
 from base import BaseDataLoader
 
+""" Dataset partitioning helper """
+class Partition(object):
+
+    def __init__(self, data, index):
+        self.data = data
+        self.index = index
+
+    def __len__(self):
+        return len(self.index)
+
+    def __getitem__(self, index):
+        data_idx = self.index[index]
+        return self.data[data_idx]
+
+
+class DataPartitioner(object):
+
+    def __init__(self, data, sizes=[0.7, 0.2, 0.1], sum_partition_size=None, seed=1234):
+        self.data = data
+        self.partitions = []
+        rng = Random()
+        rng.seed(seed)
+        data_len = len(data)
+        if sum_partition_size is not None:
+            data_len = int(data_len / sum_partition_size) * sum_partition_size
+        # data_len = sum([int(data_len * s) for s in sizes]) # Laurence 20230611
+        indexes = [x for x in range(0, data_len)]
+        rng.shuffle(indexes)
+
+        for frac in sizes:
+            part_len = int(frac * data_len)
+            self.partitions.append(indexes[0:part_len])
+            indexes = indexes[part_len:]
+
+    def use(self, partition):
+        return Partition(self.data, self.partitions[partition])
+    
+
+    
 
 class MnistDataLoader(BaseDataLoader):
     """
